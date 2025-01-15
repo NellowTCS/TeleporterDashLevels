e23a595b1a2c13e34dd0634758cc54540ff1d266
NellowTCS <neeljaiswal23@gmail.com>
2025-01-15 14:35:43 -0700

    Add level0_20250115213543253.js

---
+++ level0_20250115213543253.js
@@ -0,0 +1,72 @@
+// Level 0: Hardest Level Ever
+window.levelData = {
+    id: 0,
+    title: "Hardest Level Ever",
+    author: "Unknown Author",
+    difficulty: "Super Demon",
+    matrix: [
+    [
+        -3,
+        -3
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ],
+    [
+        0,
+        4
+    ]
+],
+    music: "../Sound/Level Soundtracks/level4.mp3",
+    colorTransitionDuration: 0.5, 
+    colorTransitionDelay: 0.1    
+};
